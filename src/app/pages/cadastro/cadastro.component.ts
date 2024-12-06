4a3a6e0eb31fb74e6dd4a348bbbafefbbaac27ab
Guilherme Moraes <guighm@outlook.com.br>
2024-12-06 02:13:40 -0400

    .

--- src/app/pages/cadastro/cadastro.component.ts
+++ src/app/pages/cadastro/cadastro.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { FormComponent } from "../../components/form/form.component";
+import { FooterComponent } from "../../components/footer/footer.component";
 
 @Component({
   selector: 'app-cadastro',
-  imports: [],
+  imports: [FormComponent, FooterComponent],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.css'
 })
